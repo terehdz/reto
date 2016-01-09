a7d50062d59920f9cf2249dd4ea93f94e7c868c0
terehdz <enismay@gmail.com>
2016-01-09 14:10:57 -0600

    prueba
    prueba

---
+++ Contents.swift
@@ -0,0 +1,22 @@
+//: Tarea
+
+
+import UIKit
+
+
+
+for num in 0...100{
+    if num % 5 == 0{
+        print("\(num) : Bingo!!")
+    }
+    if num % 2 == 0{
+        print("\(num) : Es par!!")
+    }
+    else{
+        print("\(num) : Es impar!!")
+    }
+    if num >= 30 && num <= 40{
+        print("\(num) : Viva Swift!!")
+    }
+}
+
